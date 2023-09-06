b0bd232b9484ee9ed6af150cbf208961afc0dfb0
Victor Dantas <dev.victordantas@gmail.com>
2023-09-06 16:31:24 -0300

    added data-action

--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -2,6 +2,10 @@ import { controls } from "./elements.js"
 
 export function registerControls() {
   controls.addEventListener("click", event => {
-    console.log(event.target)
+    const action = event.target.dataset.action
+    if (action === undefined) {
+      return
+      // event.target se refere ao evento de clickar e dataset refere-se ao acesso do evento no data definido no HTMl e action o atributo, por isso dataset.action
+    }
   })
 }
