14908b64a5f54e98f057d5e25e8c86995b12d6b4
Victor Dantas <dev.victordantas@gmail.com>
2023-09-25 12:03:54 -0300

    Novas modificações

--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -3,7 +3,10 @@ import { buttonSounds } from "./elements.js"
 
 export function registerControls() {
   controls.addEventListener("click", event => {
-    console.log(event.target)
+    const action = event.target.dataset.action
+    if (action === undefined) {
+      return
+    }
   })
 }
 
